e755d521e069d7a7264e0400d937db9e2f0454ff
marti <martisimonrojas@gmail.com>
2026-01-12 01:29:16 +0100

    k

---
+++ lib/screen_space.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:milestone3/tree.dart';
+import 'package:milestone3/screen_partition.dart';
+
+class ScreenSpace extends StatefulWidget {
+
+  final String id;
+
+  const ScreenSpace({super.key, required this.id});
+
+  @override
+  State<ScreenSpace> createState() => _StateScreenSpace();
+}
+
+class _StateScreenSpace extends State<ScreenSpace> {
+  late Tree tree;
+  late Area area;
+
+  Widget _buildRow(Door door, int index) {
+
+      return ListTile(
+        title: Text('${door.id}'),
+        trailing: Text('${door.state}, closed=${door.closed}'),
+      );
+    }
+
+  @override
+  void initState() {
+    super.initState();
+    tree = getTree(widget.id);
+  }
+
+  Widget _buildRow(Area area, int index) {
+    assert (area is Partition || area is Space);
+    if (area is Partition) {
+      return ListTile(
+        title: Text('P ${area.id}'),
+        onTap: () => _navigateDownPartition(area.id),
+        // TODO, navigate down to show children areas
+      );
+    } else {
+      return ListTile(
+        title: Text('S ${area.id}'),
+        onTap: () => _navigateDownSpace(area.id),
+        // TODO, navigate down to show children doors
+      );
+    }
+  }
+
+  void _navigateDownPartition(String childId) {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (context) => ScreenPartition(id: childId,))
+    );
+  }
+
+  void _navigateDownSpace(String childId) {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (context) => ScreenSpace(id: childId,))
+    );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(tree.root.id),
+        actions: <Widget>[
+          IconButton(icon: const Icon(Icons.home),
+              onPressed: () {}
+            // TODO go home page = root
+          ),
+          //TODO other actions
+        ],
+      ),
+      body: ListView.separated(
+        // it's like ListView.builder() but better
+        // because it includes a separator between items
+        padding: const EdgeInsets.all(16.0),
+        itemCount: tree.root.children.length,
+        itemBuilder: (BuildContext context, int index) =>
+            _buildRow(tree.root.children[index], index),
+        separatorBuilder: (BuildContext context, int index) =>
+        const Divider(),
+      ),
+    );
+  }
+
+}
